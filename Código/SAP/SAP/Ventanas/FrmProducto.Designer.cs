225d0f4329dc3156ee8a43d549e3a0801d910d64
HeberCRdz <heberrdz47@gmail.com>
2018-11-26 17:18:24 -0600

    Agregar, Consultar, Modificar,

--- Código/SAP/SAP/Ventanas/FrmProducto.Designer.cs
+++ Código/SAP/SAP/Ventanas/FrmProducto.Designer.cs
@@ -28,22 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtCodigoBarras = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtMarca = new System.Windows.Forms.TextBox();
+            this.TxtDescripcion = new System.Windows.Forms.TextBox();
+            this.TxtPrecio = new System.Windows.Forms.TextBox();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.BtnCerrar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtCantidad = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo Barras:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Marca:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripcion:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio:";
+            // 
+            // TxtCodigoBarras
+            // 
+            this.TxtCodigoBarras.Location = new System.Drawing.Point(115, 5);
+            this.TxtCodigoBarras.Name = "TxtCodigoBarras";
+            this.TxtCodigoBarras.Size = new System.Drawing.Size(172, 20);
+            this.TxtCodigoBarras.TabIndex = 5;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(115, 47);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(172, 20);
+            this.TxtNombre.TabIndex = 6;
+            // 
+            // TxtMarca
+            // 
+            this.TxtMarca.Location = new System.Drawing.Point(115, 86);
+            this.TxtMarca.Name = "TxtMarca";
+            this.TxtMarca.Size = new System.Drawing.Size(172, 20);
+            this.TxtMarca.TabIndex = 7;
+            // 
+            // TxtDescripcion
+            // 
+            this.TxtDescripcion.Location = new System.Drawing.Point(115, 133);
+            this.TxtDescripcion.Name = "TxtDescripcion";
+            this.TxtDescripcion.Size = new System.Drawing.Size(172, 20);
+            this.TxtDescripcion.TabIndex = 8;
+            // 
+            // TxtPrecio
+            // 
+            this.TxtPrecio.Location = new System.Drawing.Point(115, 174);
+            this.TxtPrecio.Name = "TxtPrecio";
+            this.TxtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.TxtPrecio.TabIndex = 9;
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(233, 264);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BtnGuardar.TabIndex = 10;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Location = new System.Drawing.Point(140, 264);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.BtnCerrar.TabIndex = 11;
+            this.BtnCerrar.Text = "Cerrar";
+            this.BtnCerrar.UseVisualStyleBackColor = true;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cantidad:";
+            // 
+            // TxtCantidad
+            // 
+            this.TxtCantidad.Location = new System.Drawing.Point(115, 214);
+            this.TxtCantidad.Name = "TxtCantidad";
+            this.TxtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.TxtCantidad.TabIndex = 13;
             // 
             // FrmProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.ClientSize = new System.Drawing.Size(320, 299);
+            this.Controls.Add(this.TxtCantidad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BtnCerrar);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.TxtPrecio);
+            this.Controls.Add(this.TxtDescripcion);
+            this.Controls.Add(this.TxtMarca);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtCodigoBarras);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmProducto";
             this.Text = "Producto";
+            this.Load += new System.EventHandler(this.FrmProducto_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtCodigoBarras;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtMarca;
+        private System.Windows.Forms.TextBox TxtDescripcion;
+        private System.Windows.Forms.TextBox TxtPrecio;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.Button BtnCerrar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtCantidad;
     }
 }
--- Código/SAP/SAP/Ventanas/FrmProducto.cs
+++ Código/SAP/SAP/Ventanas/FrmProducto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAP.BaseDeDatos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,129 @@ namespace SAP.Ventanas
 {
     public partial class FrmProducto : Form
     {
+        private Producto _modelo;
         public FrmProducto(long id = 0)
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterParent;
+
+            _modelo = new Producto();
+        }
+
+        private void FrmProducto_Load(object sender, EventArgs e)
+        {
+            if (_modelo.Id == 0)
+            {
+                Text = "Nuevo Producto";
+            }
+            else
+            {
+                Text = "Editar Producto";
+            }
+
+            _consultar();
+
+        }
+
+        private void _consultar()
+        {
+            if (_modelo.Id == 0) return;
+            if (!Producto.Consultar(ref _modelo))
+            {
+                MessageBox.Show("Error al consultar Producto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _llenarDatos();
+        }
+
+        private void _guardar()
+        {
+            if (!_validar()) return;
+            if (!Producto.Guardar(ref _modelo))
+            {
+                MessageBox.Show("Error al guardar Producto", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("Producto guardado correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            if (ModuloGeneral.UsuarioActivo.Id == _modelo.Id)
+            {
+                MessageBox.Show("Se reiniciará el Sistema para aplicar los cambios realizados", "Reinicio de sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ModuloGeneral.FrmLogin.Visible = true;
+                ModuloGeneral.FrmMDI.CerraAplicacion = false;
+                ModuloGeneral.FrmMDI.Close();
+            }
+            DialogResult = DialogResult.OK;
+        }
+
+        private void _llenarDatos()
+        {
+            TxtCodigoBarras.Text = _modelo.CodigoBarras;
+            TxtNombre.Text = _modelo.Nombre;
+            TxtMarca.Text = _modelo.Marca;
+            TxtDescripcion.Text = _modelo.Descripcion;
+            TxtPrecio.Text = _modelo.Precio.ToString();
+            TxtCantidad.Text = _modelo.Cantidad.ToString();
+
+        }
+
+        private bool _validar()
+        {
+            if (TxtCodigoBarras.Text.Length == 0)
+            {
+                MessageBox.Show("El Codigo de Barras es requerido", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (TxtNombre.Text.Length == 0)
+            {
+                MessageBox.Show("El Nombre es requerido", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (TxtMarca.Text.Length == 0)
+            {
+                MessageBox.Show("La Marca es requerida", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (TxtDescripcion.Text.Length == 0)
+            {
+                MessageBox.Show("La Descripcion es requerida", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (TxtPrecio.Text.Length == 0)
+            {
+                MessageBox.Show("El Precio es requerido", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            if (TxtCantidad.Text.Length == 0)
+            {
+                MessageBox.Show("La Cantidad es requerida", "No validado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+
+            _modelo.CodigoBarras = TxtCodigoBarras.Text;
+            _modelo.Nombre = TxtNombre.Text;
+            _modelo.Marca = TxtMarca.Text;
+            _modelo.Descripcion = TxtDescripcion.Text;
+            _modelo.Precio = float.Parse(TxtPrecio.Text);
+            _modelo.Cantidad = int.Parse(TxtCantidad.Text);
+            return true;
+
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            _guardar();
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
         }
     }
 }
